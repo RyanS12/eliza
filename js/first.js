39b6923567936af2c2f64ceac9f6da5591b77bb9
Ryan <junhosong@stmarksschool.org>
2018-04-27 09:23:29 -0400

    hi

---
+++ js/first.js
@@ -0,0 +1,43 @@
+
+
+/*
+
+
+Mad Lib Story
+
+
+*/
+
+
+function doMadLib() {
+	alert("Here is you MadLib. Ill ask you some questions");
+
+	var character= prompt("anything that is male")
+	var verb1= prompt("Give me a verb, please");
+	var say1= prompt("What would say right after you wake up?");
+	var do1 = prompt("What would you do 5min after waking up?");
+	var weather = prompt("Give me a weather, pls.");
+	var weather2 = prompt("Another Weather pls")
+	var verb2= prompt("another verb?");
+	var verb3= prompt("last verb pls");
+	var noun1 = prompt("A noun that you can see");
+	var adv1 = prompt("An adv");
+	var place = prompt("a special place")
+	var verb4 = verb3+"ed"
+
+	alert("madlib starting!");
+
+	var story = "Once upon ago, there was a <span class='c'>"+character+"</span>. He was a special <span class='c'>"+character+"</span>, <br><br> because he could <span class='c'>"
+	 + verb1 + "</span>. He was also very proud of <br><br>the fact that he could <span class='c'>" + verb2 + 
+	 "</span>. One day, he woke <br><br> up and said <span class='c'>" + say1 + "</span>. He then felt the need to <span class='c'>" + do1 + 
+	 "</span>. <br><br>Then, he <span class='c'>" + verb4 + "</span> and went out of his house into <span class='c'>" + weather + "</span>. He then <br><br> decided to <span class='c'>"
+	 +verb1+"</span>. Feeling good about himself, he went back inside to <span class='c'>"+verb3+"</span>. <br><br> But then he saw a "
+	 +noun1+"</span> standing on the dining table. Surprised, <br><br> he ran out <span class='c'>"+adv1+"</span>. "+
+	 "However, even when he went out, the <span class='c'>" +weather2+ "</span>surprised <br><br> him. " 
+	 +"It was then that he realized that <br><br> he was in <span class='c'>"+place+"</span>. ";
+
+
+
+	document.getElementById("finalMadLib").innerHTML = story;
+
+}
